abb4c3cc3f734d5e92c2e404f492fdeef5a8d7b9
Daryna Karmazin <daryna.karmazin@gmail.com>
2024-08-07 16:34:05 +0300

    Updated single.php

--- single.php
+++ single.php
@@ -106,10 +106,10 @@ endif;
         <?php endif; ?>
 
         <?php 
-        $textWidgetTitle = get_field('widget_title', get_option('page_for_posts'));
-        $textWidgetContent = get_field('widget_content', get_option('page_for_posts'));
+        $textWidgetTitle = get_field('text_widget', get_option('page_for_posts'));
+        $textWidgetContent = get_field('text_widget_content', get_option('page_for_posts'));
         if($textWidgetContent) :
-        ?>       
+        ?>     
         <li class="block">
           <?php if($textWidgetTitle ) : ?>
           <h4 class="heading"><?php echo $textWidgetTitle; ?></h4>
